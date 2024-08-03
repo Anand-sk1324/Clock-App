f16fb365dce08f212eef79a09bbfe2873915d87f
Anand-sk1324 <99873691+Anand-sk1324@users.noreply.github.com>
2024-08-03 17:57:13 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://anand-sk1324.github.io',
-  redirects: {
-    '/': '/Clock-App',
-    '/PomodoroTimer': '/Clock-App/PomodoroTimer',
-    '/PomodoroTimer/LongBreak': '/Clock-App/PomodoroWork',
-    '/PomodoroTimer/LongBreak': '/Clock-App/PomodoroTimer/LongBreak',
-    '/PomodoroTimer/ShortBreak': '/Clock-App/PomodoroTimer/ShortBreak',
-    '/StopWatch': '/Clock-App/StopWatch',
-  }
+  base: '/Clock-App/'
 });

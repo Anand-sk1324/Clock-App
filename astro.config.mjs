c781b5c26363b2fc3a5bafa35e0563dd076a867a
Anand-sk1324 <99873691+Anand-sk1324@users.noreply.github.com>
2024-08-03 12:14:36 +0530

    Update2 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://anand-sk1324.github.io',
-  base: '/Clock-App'
+  base: '/Clock-App/'
 });

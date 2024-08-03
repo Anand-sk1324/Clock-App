601d626714dc309d13b475a368697e79359db748
Anand-sk1324 <anand.sk1324@gmail.com>
2024-08-03 15:50:47 +0530

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,12 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://anand-sk1324.github.io',
-  base: '/Clock-App',
-  trailingSlash: "never"
+  redirects: {
+    '/': '/Clock-App',
+    '/PomodoroTimer': '/Clock-App/PomodoroTimer',
+    '/PomodoroTimer/LongBreak': '/Clock-App/PomodoroWork',
+    '/PomodoroTimer/LongBreak': '/Clock-App/PomodoroTimer/LongBreak',
+    '/PomodoroTimer/ShortBreak': '/Clock-App/PomodoroTimer/ShortBreak',
+    '/StopWatch': '/Clock-App/StopWatch',
+  }
 });
